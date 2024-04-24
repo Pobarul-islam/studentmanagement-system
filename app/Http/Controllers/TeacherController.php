a022110819035c2ed40de1101785d3603635d133
Pobarul-islam <pobarul3@gmail.com>
2024-04-24 09:21:40 +0600

    teacher db model controller added and page showed

---
+++ app/Http/Controllers/TeacherController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Teacher;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class TeacherController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function index(): View
+    {
+        $students = Teacher::all();
+        return view('teachers.index')->with('teachers', $students);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('teachers.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        Teacher::create($input);
+        return redirect('teachers')->with('success', 'Successfully created!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id) : View
+    {
+        $teacher = Teacher::find($id);
+        return view('teachers.show')->with('teachers', $teacher);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
