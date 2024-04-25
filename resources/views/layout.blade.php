97e2ae6514199232524bc87040dab320069fa408
Pobarul-islam <pobarul3@gmail.com>
2024-04-25 20:27:52 +0600

    courses toggled in batch pages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -85,7 +85,7 @@
         <div class="row">
             <div class="col-12">
                 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Student Management System</a>
+                    <h1 class="navbar-brand" href="#">Student Management System</h1>
                     <button class="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
